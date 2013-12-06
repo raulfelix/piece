43399046046c376edb237d25853333e30d137452
Raul Carrizo <raul.carrizo@fluidintel.com>
2013-12-06 22:03:14 +1100

    updated grunt file to compress build css

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
     sass: {                             
       dist: {                           
         options: {                 
-          style: 'compact'
+          style: 'compressed'
         },
         files: {
           'style.css': 'sass/master.scss'
